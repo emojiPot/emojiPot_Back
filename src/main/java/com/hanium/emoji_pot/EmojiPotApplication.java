7ac3a2be085661be6797be1650d9227c3b7c533f
heejung <gimm9567@gmail.com>
2023-07-01 00:34:02 +0900

    feat: 초기 세팅

---
+++ src/main/java/com/hanium/emoji_pot/EmojiPotApplication.java
@@ -0,0 +1,13 @@
+package com.hanium.emoji_pot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojiPotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmojiPotApplication.class, args);
+	}
+
+}
