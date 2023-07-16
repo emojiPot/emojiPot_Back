17905ca0a2a35ef7412a292d5f35136a747fac20
heejung <gimm9567@gmail.com>
2023-07-17 01:09:52 +0900

    feat: Entity 작성

--- src/main/java/com/hanium/emoji_pot/domain/posts/Post.java
+++ src/main/java/com/hanium/emoji_pot/domain/posts/Post.java
@@ -1,39 +1,60 @@
 package com.hanium.emoji_pot.domain.posts;
 
 import com.hanium.emoji_pot.domain.BaseTimeEntity;
+import com.hanium.emoji_pot.domain.posts.dto.PostRequestDto;
+import com.hanium.emoji_pot.domain.posts.dto.PostUpdateRequestDto;
+import com.hanium.emoji_pot.domain.users.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Table(name = "post")
 @NoArgsConstructor
 public class Post extends BaseTimeEntity {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long postId;
 
-    private Long userId;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
     @Column(length = 30)
     private String location;
 
+    @Column(nullable = false)
     private Integer emotion;
 
+    @Column(nullable = false)
     private String record;
 
-    @Column(name = "is_deleted")
+    @Column(nullable = false)
     private boolean isDeleted;
 
     @Builder
-    public Post(Long userId, String location, Integer emotion, String record) {
-        this.userId = userId;
+    public Post(User user, String location, Integer emotion, String record) {
+        this.user = user;
         this.location = location;
         this.emotion = emotion;
         this.record = record;
         this.isDeleted = false;
+    }
+
+    public void deletePost() {
+        this.isDeleted = true;
+    }
+
+    public void updatePost(PostUpdateRequestDto postUpdateRequestDto) {
+        this.location = postUpdateRequestDto.getLocation();
+        this.emotion = postUpdateRequestDto.getEmotion();
+        this.record = postUpdateRequestDto.getRecord();
     }
 }
