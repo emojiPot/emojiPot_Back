4c198dc5774b67634474e6a47e5aa186a0fb53ff
heejung <gimm9567@gmail.com>
2023-08-04 19:43:06 +0900

    feat: BaseTimeEntity 클래스 수정

--- src/main/java/com/hanium/emoji_pot/domain/BaseTimeEntity.java
+++ src/main/java/com/hanium/emoji_pot/domain/BaseTimeEntity.java
@@ -15,10 +15,10 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
     @CreatedDate
-    @Column(name="created_at")
+    @Column(name="created_at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
 
     @LastModifiedDate
-    @Column(name="updated_at")
+    @Column(name="updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
