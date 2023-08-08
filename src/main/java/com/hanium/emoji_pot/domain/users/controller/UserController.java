d839d6fe1fd1d1f29f23982b4918851d45fb4b65
heejung <gimm9567@gmail.com>
2023-08-08 17:24:52 +0900

    feat: 사용자 컨트롤러 수정

--- src/main/java/com/hanium/emoji_pot/domain/users/controller/UserController.java
+++ src/main/java/com/hanium/emoji_pot/domain/users/controller/UserController.java
@@ -6,12 +6,20 @@ import com.hanium.emoji_pot.domain.users.dto.UserLoginResponseDto;
 import com.hanium.emoji_pot.domain.users.dto.UserRegisterRequestDto;
 import com.hanium.emoji_pot.domain.users.dto.UserRegisterResponseDto;
 import com.hanium.emoji_pot.domain.users.service.UserService;
+import com.hanium.emoji_pot.global.exception.ExceptionManager;
+import com.hanium.emoji_pot.global.exception.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.sql.SQLException;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -23,16 +31,42 @@ public class UserController {
     private final BCryptPasswordEncoder encoder;
 
     @PostMapping("/register")
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserRegisterResponseDto register(@Validated @RequestBody UserRegisterRequestDto registerRequest) {
-        User register = userService.register(registerRequest.toEntity(encoder.encode(registerRequest.getPassword())));
-        return UserRegisterResponseDto.of(register);
+    public ResponseEntity register(@Validated @RequestBody UserRegisterRequestDto registerRequest, BindingResult br) throws SQLException {
+        log.info("🎉 회원 가입 요청 requestDto : {}", registerRequest);
+
+        if (br.hasErrors()) {
+            ExceptionManager.ifNullAndBlank();
+        }
+
+        UserRegisterResponseDto registerResponse = userService.register(registerRequest);
+
+        return ResponseEntity.ok(Response.success(registerResponse));
     }
 
     @PostMapping("/login")
-    @ResponseStatus(HttpStatus.OK)
-    public UserLoginResponseDto login(@Validated @RequestBody UserLoginRequestDto loginRequest) {
-        String token = userService.login(loginRequest.getEmail(), loginRequest.getPassword());
-        return new UserLoginResponseDto(token);
+    public ResponseEntity login(@Validated @RequestBody UserLoginRequestDto loginRequest, BindingResult br, HttpServletRequest request) throws SQLException {
+        log.info("🎉 로그인 요청 requestDto : {}", loginRequest);
+
+        if (br.hasErrors()) {
+            ExceptionManager.ifNullAndBlank();
+        }
+
+        UserLoginResponseDto loginResponse = userService.login(loginRequest);
+
+        HttpSession session = request.getSession(true);
+        session.setAttribute("Email", loginRequest.getEmail());
+
+        return ResponseEntity.ok(Response.success(loginResponse));
     }
+
+//    @PostMapping("/{user_id}/role/change")
+//    public ResponseEntity changeRole(@PathVariable(name = "user_id") Long userId) {
+//        log.info("🎉 관리자가 등급을 변경할 회원 id : {} ", userId);
+//
+//        userService.changeRole(userId);
+//
+//        UserRoleChangeResponseDto responseDto = new UserRoleChangeResponseDto(userId, userId + "번 아이디의 권한을 변경하였습니다.");
+//
+//        return ResponseEntity.ok(Response.success(responseDto));
+//    }
 }
