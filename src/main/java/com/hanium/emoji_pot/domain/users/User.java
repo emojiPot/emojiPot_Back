9d0c594abb97b2032932f4a0c5eff951af8fb9cb
heejung <gimm9567@gmail.com>
2023-08-08 17:23:49 +0900

    feat: 사용자 entity 수정

--- src/main/java/com/hanium/emoji_pot/domain/users/User.java
+++ src/main/java/com/hanium/emoji_pot/domain/users/User.java
@@ -1,12 +1,16 @@
 package com.hanium.emoji_pot.domain.users;
 
 import com.hanium.emoji_pot.domain.BaseTimeEntity;
+import com.hanium.emoji_pot.domain.posts.Post;
+import com.hanium.emoji_pot.domain.users.dto.UserRegisterRequestDto;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -37,15 +41,30 @@ public class User extends BaseTimeEntity {
 
     private Boolean isDeleted;
 
-    @Builder
-    public User(String name, String username, String email, String password, String profileImage, String introduce) {
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts = new ArrayList<>();
+
+    public User(String name, String username, String email, String password) {
         this.name = name;
         this.username = username;
         this.email = email;
         this.password = password;
-        this.profileImage = profileImage;
-        this.introduce = introduce;
         this.isDeleted = false;
+        this.role = UserRole.ROLE_USER;
+    }
+
+    public static User createUser(UserRegisterRequestDto registerRequest, String password) {
+        return new User(registerRequest.getName(), registerRequest.getUsername(), registerRequest.getEmail(), password);
+    }
+    public void changeRole() {
+        if (this.role.equals(UserRole.ROLE_USER)) {
+            this.role = UserRole.ROLE_ADMIN;
+        } else {
+            this.role = UserRole.ROLE_USER;
+        }
     }
 
 }
