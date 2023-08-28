f8850ca81329400027c251010fd65761e2ceeb17
heejung <gimm9567@gmail.com>
2023-08-29 02:59:19 +0900

    feat: 댓글 작성 기능 구현

---
+++ src/main/java/com/hanium/emoji_pot/domain/comments/controller/CommentController.java
@@ -0,0 +1,42 @@
+package com.hanium.emoji_pot.domain.comments.controller;
+
+import com.hanium.emoji_pot.domain.comments.dto.CommentRequestDto;
+import com.hanium.emoji_pot.domain.comments.dto.CommentResponseDto;
+import com.hanium.emoji_pot.domain.comments.service.CommentService;
+import com.hanium.emoji_pot.global.exception.ExceptionManager;
+import com.hanium.emoji_pot.global.exception.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+
+@RestController
+@RequestMapping("/v1/posts")
+@RequiredArgsConstructor
+@Slf4j
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{postId}/comments")
+    public ResponseEntity saveComment(@PathVariable("postId") Long postId, @Validated @RequestBody CommentRequestDto commentRequest, BindingResult br, Authentication authentication) throws SQLException {
+        log.info("댓글을 작성하려는 게시글 id : {} || requestDto : {}", postId, commentRequest);
+
+        if (br.hasErrors()) {
+            return ExceptionManager.ifNullAndBlank();
+        }
+
+        String requestUserEmail = authentication.getName();
+
+        log.info("댓글 작성 요청자 Email : {} ", requestUserEmail);
+
+        CommentResponseDto commentResponse = commentService.saveComment(commentRequest, requestUserEmail, postId);
+
+        return ResponseEntity.ok(Response.success(commentResponse));
+    }
+}
---
+++ src/main/java/com/hanium/emoji_pot/domain/comments/service/CommentService.java
@@ -0,0 +1,61 @@
+package com.hanium.emoji_pot.domain.comments.service;
+
+import com.hanium.emoji_pot.domain.comments.Comment;
+import com.hanium.emoji_pot.domain.comments.CommentRepository;
+import com.hanium.emoji_pot.domain.comments.dto.CommentRequestDto;
+import com.hanium.emoji_pot.domain.comments.dto.CommentResponseDto;
+import com.hanium.emoji_pot.domain.posts.Post;
+import com.hanium.emoji_pot.domain.posts.PostRepository;
+import com.hanium.emoji_pot.domain.users.User;
+import com.hanium.emoji_pot.domain.users.UserRepository;
+import com.hanium.emoji_pot.global.exception.AppException;
+import com.hanium.emoji_pot.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional(readOnly = true)
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+
+    @Transactional
+    public CommentResponseDto saveComment(CommentRequestDto commentRequest, String requestUserEmail, Long postId) throws SQLException {
+        User user = userValid(requestUserEmail);
+        Post post = postValid(postId);
+
+        Comment comment = Comment.createComment(commentRequest.getContent(), user, post);
+        commentRepository.save(comment);
+
+        return new CommentResponseDto(comment, requestUserEmail, postId);
+    }
+
+    public User userValid(String email) {
+        return userRepository.findByEmailAndIsDeleted(email, false)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+    }
+
+    public Post postValid(Long postId) {
+        Post post = postRepository.findByPostIdAndIsDeleted(postId, false)
+                .orElseThrow(() -> new AppException(ErrorCode.POST_NOT_FOUND));
+
+        if (post.getIsDeleted()) {
+            throw new AppException(ErrorCode.POST_NOT_FOUND);
+        }
+        return post;
+    }
+
+    public Comment commentValid(Long commentId) {
+        return commentRepository.findById(commentId)
+                .orElseThrow(() -> new AppException(ErrorCode.COMMENT_NOT_FOUND));
+    }
+
+}
